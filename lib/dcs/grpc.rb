29552a18d6de4ed780e61865d4d4a383539aa87c
Jeffrey Jones <jeff@jones.be>
2021-01-05 22:54:13 +0900

    Initial setup
    Initial setup of the repository. Test folder will be added later with the first
    actual code.

---
+++ lib/dcs/grpc.rb
@@ -0,0 +1,8 @@
+require "dcs/grpc/version"
+
+module Dcs
+  module Grpc
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/dcs/grpc/version.rb
@@ -0,0 +1,5 @@
+module Dcs
+  module Grpc
+    VERSION = "0.1.0"
+  end
+end
